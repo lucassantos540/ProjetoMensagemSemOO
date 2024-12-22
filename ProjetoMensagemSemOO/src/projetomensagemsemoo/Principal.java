e3ba4e45d558e9d3161ab57c392c9a273d88244b
Lucas Santos <100425690+lucassantos540@users.noreply.github.com>
2024-12-22 19:32:56 -0300

    Pasta do projeto

---
+++ ProjetoMensagemSemOO/src/projetomensagemsemoo/Principal.java
@@ -0,0 +1,17 @@
+package projetomensagemsemoo;
+
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Dados Cadastrados!");
+        JOptionPane.showMessageDialog(null, "Login/Senha Incorretos!");
+        JOptionPane.showMessageDialog(null, "Senha já Cadastrada!"); 
+        
+    }
+    
+}
